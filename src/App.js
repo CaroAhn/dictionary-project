eaf19aeecb9ee58c03b5864ff5ff280d812575fb
CaroAhn <ahncaro93@gmail.com>
2021-11-10 10:52:59 -0500

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hola mundo</header>
+      <div className="container">
+        <header className="App-header">hola mundo</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Caro Ahn</footer>
+      </div>
     </div>
   );
 }
