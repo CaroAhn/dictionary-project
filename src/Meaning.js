9a12a90ff37552db9643e705a0758b8bd72b5873
CaroAhn <ahncaro93@gmail.com>
2021-11-13 13:13:15 -0500

    Styled the dictionary app

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,13 +9,12 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <div className="meaning-definition">{definition.definition}</div>
+
+            <div className="meaning-example">
+              <em>{definition.example}</em>{" "}
+            </div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
