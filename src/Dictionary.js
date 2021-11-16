aa9824c7880b7b8ac0162cd56c0e339aa0e4ac66
CaroAhn <ahncaro93@gmail.com>
2021-11-16 12:27:17 -0500

    Added Photos component and authorization for api photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("hope");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001eda82b3f0ca1412fa27f794f7c89235f";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -43,9 +58,10 @@ export default function Dictionary() {
               placeholder="Enter a word"
             />
           </form>
-          <div className="hint">e.g., flower, love, planet, mammoth</div>
+          <div className="hint">e.g., flower, galaxy, planet, mammoth</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
